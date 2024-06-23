4911dc099875150ff7898d208d0043f603efab46
Chandra_Mouliesh_N <chandra.n@nouveau-labs.com>
2024-06-23 14:17:40 +0530

    crud apis demo DA0

---
+++ 02-CrudApisDemo/src/main/java/com/luv2code/springbott/crudapisdemo/CrudapisdemoApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.springbott.crudapisdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudapisdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudapisdemoApplication.class, args);
+	}
+
+}
---
+++ 02-CrudApisDemo/src/test/java/com/luv2code/springbott/crudapisdemo/CrudapisdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.springbott.crudapisdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudapisdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
